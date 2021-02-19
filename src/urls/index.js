bc22dba169fddba426fd7f7a42ea9596286850cf
Dulaya Saennok <dulaya.saennok@gmail.com>
2021-02-19 15:43:19 -0800

    minor change

--- src/urls/index.js
+++ src/urls/index.js
@@ -1,6 +1,6 @@
 
 
-export const url = process.env.REACT_APP_BASE_URL //|| 'http://localhost:5000/api/' ;//'http://localhost:5000/api/'; /*5000 for local development only*/
+export const url = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/api/' ;//'http://localhost:5000/api/'; /*5000 for local development only*/
 
 export const registerURL = url + 'user/register';
 export const loginURL = url + 'user/login';
